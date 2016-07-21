3435d963ce89b5afae5fe423727943e053c2765d
John Carmichael <jlcarmic@gmail.com>
2016-07-21 14:50:52 -0400

    Added salary checking to validateLineup
    * Added a private function for checking lineup salary against context max value
    * Added error capture/return for validateLineup method for reporting all reasons a lineup is invalid
    * Added call to validateMaximumSalary witin validateLineup
    * Added test structure as well as test for validateMaximumSalary
    * Added first test cases for validateLineup specifc to salary requirements

--- Contest.js
+++ Contest.js
@@ -1,18 +1,25 @@
 function Contest(data) {
   this.maxSalary = data.maxSalary === undefined ? 50000 : data.maxSalary;
-  this.positionCounts = data.positionCounts === undefined ? {} : data.positionCounts;
   this.minGames = data.minGames === undefined ? 2 : data.minGames;
+  this.positionCounts = data.positionCounts === undefined ? {} : data.positionCounts;
   this.totalFromTeam = data.totalFromTeam === undefined ? 3 : data.totalFromTeam;
 }
 
 // Public functions
 Contest.prototype.validateLineup = function(lineup) {
+  var errors = [];
 
+  if(!validateMaximumSalary(lineup, this.maxSalary)) {
+    errors.push(new Error("Lineup salary exceeds maximum salary for this contest"));
+  }
+
+  return errors;
 };
 
 // Private functions
-function validateSalary(lineup) {
-  return (lineup.calculateTotalSalary() > this.maxSalary) ? false : true;
+function validateMaximumSalary(lineup, maxSalary) {
+  return (lineup.calculateTotalSalary() > maxSalary) ? false : true;
+}
 }
 
 module.exports = Contest;
--- test/contest_test.js
+++ test/contest_test.js
@@ -1,2 +1,67 @@
 var expect = require('chai').expect;
-var Contest = require('../Contest');
+var Lineup = require('../Lineup');
+var Player = require('../Player');
+var rewire = require('rewire');
+
+var Contest = rewire('../Contest');
+
+describe("Contest", function() {
+  var lineup, player1, player2, priv_validateMaximumSalary;
+  before(function() {
+    player1 = new Player({
+      "id": 58,
+      "name": "John Carmichael",
+      "position": "LB",
+      "salary": 5000,
+      "gameId": 20
+    });
+    player2 = new Player({
+      "id": 11,
+      "name": "Alex Smith",
+      "position": "QB",
+      "salary": 6000,
+      "gameId": 21
+    });
+
+    lineup = new Lineup();
+    lineup.addPlayer(player1);
+    lineup.addPlayer(player2);
+
+    // Rewire private methods for testing
+    priv_validateMaximumSalary = Contest.__get__('validateMaximumSalary');
+  });
+
+  describe("validateLineup", function() {
+    it("returns an empty array when all lineup validations check out as there are no errors to report", function(done) {
+      var contest = new Contest({ "maxSalary": 50000, "positionCounts": {}, "minGames": 2, "totalFromTeam": 3 });
+
+      expect(contest.validateLineup(lineup)).to.eql([]);
+      done();
+    });
+
+    it("returns an array of error objects, one for each failed lineup validation", function(done) {
+      var contest = new Contest({ "maxSalary": 5000, "positionCounts": {}, "minGames": 2, "totalFromTeam": 3 });
+      var errors = [];
+
+      errors.push(new Error("Lineup salary exceeds maximum salary for this contest"));
+      expect(contest.validateLineup(lineup)).to.eql(errors);
+      done();
+    });
+  });
+
+  describe("validateMaximumSalary", function() {
+    it("returns true when the total salary of all players in the lineup is less than the maximum salary value of the contest", function(done) {
+      var contest = new Contest({ "maxSalary": 50000, "positionCounts": {}, "minGames": 2, "totalFromTeam": 3 });
+
+      expect(priv_validateMaximumSalary(lineup)).to.eql(true);
+      done();
+    });
+
+    it("returns false when the total salary of all players in the lineup exceeds the maximum salary value of the contest", function(done) {
+      var contest = new Contest({ "maxSalary": 5000, "positionCounts": {}, "minGames": 2, "totalFromTeam": 3 });
+
+      expect(priv_validateMaximumSalary(lineup)).to.eql(true);
+      done();
+    });
+  });
+});
