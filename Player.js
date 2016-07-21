6443541a29c75c38ddcca12486f8ad3c7f6b7576
John Carmichael <jlcarmic@gmail.com>
2016-07-21 14:45:40 -0400

    Made player values required
    * Player id, name, position, salary and gameId are now required
    * Updated tests based on changes to required params

--- Player.js
+++ Player.js
@@ -1,8 +1,15 @@
 function Player(data) {
-  this.id = data.id === undefined ? 0 : data.id;
-  this.name = data.name === undefined ? '' : data.name;
-  this.position = data.position === undefined ? '' : data.position;
-  this.salary = data.salary === undefined ? 0 : data.salary;
+  if(data.id === undefined || data.name === undefined ||
+    data.position === undefined || data.salary === undefined ||
+    data.gameId === undefined) {
+      throw new Error("Invalid Parameters");
+  } else {
+    this.id = data.id;
+    this.name = data.name;
+    this.position = data.position;
+    this.salary = data.salary;
+    this.gameId = data.gameId;
+  }
 }
 
 module.exports = Player;
--- test/player_test.js
+++ test/player_test.js
@@ -8,27 +8,25 @@ describe("Player", function() {
         "id": 58,
         "name": "John Carmichael",
         "position": "LB",
-        "salary": 5000
+        "salary": 5000,
+        "gameId": 20
       });
 
       expect(player.id).to.eql(58);
       expect(player.name).to.eql('John Carmichael');
       expect(player.position).to.eql('LB');
       expect(player.salary).to.eql(5000);
+      expect(player.gameId).to.eql(20);
       done();
     });
 
-    it("creates a valid player using defaults for missing data", function (done) {
-      var player = new Player({
-        "id": 58,
-        "name": "John Carmichael",
-        "position": "LB",
-      });
-
-      expect(player.id).to.eql(58);
-      expect(player.name).to.eql('John Carmichael');
-      expect(player.position).to.eql('LB');
-      expect(player.salary).to.eql(0);
+    it("throws an error if the data is missing a required parameter", function (done) {
+      expect(function() {
+        Player({
+          "name": "John Carmichael",
+          "position": "LB",
+        });
+      }).to.throw("Invalid Parameters");
       done();
     });
   });
