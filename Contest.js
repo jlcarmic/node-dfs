d81b8d7260271c5434cee653bd7bc77396194150
John Carmichael <jlcarmic@gmail.com>
2016-07-23 14:40:21 -0400

    Added validatePositionCounts to Contest
    * Added function to test that the position counts for a lineup is an exact match to the contest setting
    * Added validatePositionCounts check to validateLineup for proper error reporting
    * Added/Updated test to match changes

--- Contest.js
+++ Contest.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 function Contest(data) {
   this.maxFromTeam = data.maxFromTeam === undefined ? 3 : data.maxFromTeam;
   this.maxSalary = data.maxSalary === undefined ? 50000 : data.maxSalary;
@@ -19,6 +21,10 @@ Contest.prototype.validateLineup = function(lineup) {
 
   if(!validateMinimumGames(lineup, this.minGames)) {
     errors.push(new Error("Unique games in lineup is less than the minimum games for this contest"));
+  }
+
+  if(!validatePositionCounts(lineup, this.positionCounts)) {
+    errors.push(new Error("Lineup must not have greater than or fewer players at any position than the number indicated in settings"));
   }
 
   return errors;
@@ -43,6 +49,12 @@ function validateMaximumSalary(lineup, maxSalary) {
 
 function validateMinimumGames(lineup, minGames) {
   return lineup.getGameList().length >= minGames;
+}
+
+function validatePositionCounts(lineup, contestPositionCounts) {
+  var teamPositionCounts = lineup.getPositionCounts();
+
+  return _.isEqual(contestPositionCounts, teamPositionCounts);
 }
 
 module.exports = Contest;
--- test/contest_test.js
+++ test/contest_test.js
@@ -32,23 +32,26 @@ describe("Contest", function() {
     priv_validateMaximumFromTeams = Contest.__get__('validateMaximumFromTeams');
     priv_validateMaximumSalary = Contest.__get__('validateMaximumSalary');
     priv_validateMinimumGames = Contest.__get__('validateMinimumGames');
+    priv_validatePositionCounts = Contest.__get__('validatePositionCounts');
   });
 
   describe("validateLineup()", function() {
     it("returns an empty array when all lineup validations check out as there are no errors to report", function(done) {
-      var contest = new Contest({ "maxFromTeam": 2, "maxSalary": 50000, "minGames": 2, "positionCounts": {} });
+      var contest = new Contest({ "maxFromTeam": 2, "maxSalary": 50000, "minGames": 2, "positionCounts": { "LB": 1, "QB": 1 } });
 
       expect(contest.validateLineup(lineup)).to.eql([]);
       done();
     });
 
     it("returns an array of error objects, one for each failed lineup validation", function(done) {
-      var contest = new Contest({ "maxFromTeam": 1, "maxSalary": 5000, "minGames": 3, "positionCounts": {} });
+      var contest = new Contest({ "maxFromTeam": 1, "maxSalary": 5000, "minGames": 3, "positionCounts": { "LB": 1 } });
       var errors = [];
 
       errors.push(new Error("Lineup exceeds the maximum number of players from a single team for this contest"));
       errors.push(new Error("Lineup salary exceeds maximum salary for this contest"));
       errors.push(new Error("Unique games in lineup is less than the minimum games for this contest"));
+      errors.push(new Error("Lineup must not have greater than or fewer players at any position than the number indicated in settings"));
+
       expect(contest.validateLineup(lineup)).to.eql(errors);
       done();
     });
@@ -98,6 +101,36 @@ describe("Contest", function() {
       var contest = new Contest({ "maxFromTeam": 3, "maxSalary": 5000, "minGames": 3, "positionCounts": {} });
 
       expect(priv_validateMinimumGames(lineup, contest.minGames)).to.eql(false);
+      done();
+    });
+  });
+
+  describe("validatePositionCounts()", function() {
+    it("returns true when the lineup has the exact number of players in each position as indicated in the positionCounts setting of the contest", function(done) {
+      var contest = new Contest({ "maxFromTeam": 3, "maxSalary": 5000, "minGames": 2, "positionCounts": { "LB": 1, "QB": 1 } });
+
+      expect(priv_validatePositionCounts(lineup, contest.positionCounts)).to.eql(true);
+      done();
+    });
+
+    it("returns false when the lineup has fewer players at a position than the number indicated in the positionCounts setting of the contest", function(done) {
+      var contest = new Contest({ "maxFromTeam": 3, "maxSalary": 5000, "minGames": 2, "positionCounts": { "LB": 1, "QB": 1, "TE": 1 } });
+
+      expect(priv_validatePositionCounts(lineup, contest.positionCounts)).to.eql(false);
+      done();
+    });
+
+    it("returns false when the lineup has more players at a position than the number indicated in the positionCounts setting of the contest", function(done) {
+      var contest = new Contest({ "maxFromTeam": 3, "maxSalary": 5000, "minGames": 2, "positionCounts": { "LB": 0, "QB": 1 } });
+
+      expect(priv_validatePositionCounts(lineup, contest.positionCounts)).to.eql(false);
+      done();
+    });
+
+    it("returns false when the lineup has a player in a position that isn't present in the positionCounts setting of the contest", function(done) {
+      var contest = new Contest({ "maxFromTeam": 3, "maxSalary": 5000, "minGames": 2, "positionCounts": { "LB": 1 } });
+
+      expect(priv_validatePositionCounts(lineup, contest.positionCounts)).to.eql(false);
       done();
     });
   });
